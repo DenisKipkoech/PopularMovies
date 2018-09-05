eab39b4d79ad7699dd70c3445fef18b7577ebca5
Denis <d3niskip@gmail.com>
2018-09-05 15:13:38 +0300

    fix: added data binding to movie detail

--- app/src/main/java/com/example/denis/popularmoviesstages1/MovieDetail.java
+++ app/src/main/java/com/example/denis/popularmoviesstages1/MovieDetail.java
@@ -1,36 +1,45 @@
 package com.example.denis.popularmoviesstages1;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.denis.popularmoviesstages1.data.Movie;
+import com.example.denis.popularmoviesstages1.databinding.ActivityMovieDetailBinding;
 import com.squareup.picasso.Picasso;
 
 public class MovieDetail extends AppCompatActivity {
-    private TextView releaseView, ratingView, synopsisView, titleView;
+
     private ImageView posterView;
+    private Movie movie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        titleView = findViewById(R.id.title_view);
-        releaseView = findViewById(R.id.release_date_view);
-        ratingView = findViewById(R.id.rating_view);
-        synopsisView = findViewById(R.id.movie_synopsis_view);
         posterView = findViewById(R.id.poster_view);
+        ActivityMovieDetailBinding binder = DataBindingUtil.setContentView(this,
+                R.layout.activity_movie_detail);
+
         Intent intent = getIntent();
         if (intent != null){
-            titleView.setText(intent.getStringExtra(String.valueOf(R.string.intent_title)));
-            releaseView.setText(intent.getStringExtra(String.valueOf(R.string.intent_release)));
-            ratingView.setText(intent.getStringExtra(String.valueOf(R.string.intent_rating)));
-            synopsisView.setText(intent.getStringExtra(String.valueOf(R.string.intent_plot)));
+            movie = new Movie(
+                    intent.getStringExtra(String.valueOf(R.string.intent_title)),
+                    intent.getStringExtra(String.valueOf(R.string.intent_poster)),
+                    intent.getStringExtra(String.valueOf(R.string.intent_plot)),
+                    intent.getStringExtra(String.valueOf(R.string.intent_rating)),
+                    intent.getStringExtra(String.valueOf(R.string.intent_rating))
+            );
+
+            binder.setMovie(movie);
 
             Picasso.with(getApplicationContext())
                     .load(intent.getStringExtra(String.valueOf(R.string.intent_poster)))
                     .into(posterView);
+
+
         }
 
 
